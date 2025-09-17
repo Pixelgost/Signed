e1fa884568df2c4c0f988d676e72b3f1ce13a6b1
pixelgost <thisisadhi@gmail.com>
2025-09-17 16:58:22 -0400

    Created frontend and backend and added ping endpoint

---
+++ Signed-Backend/SignedBackend/SignedBackend/views.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse
+from django.utils import timezone
+import json
+
+
+def ping(request):
+    """
+    API endpoint that returns the current timestamp.
+    """
+    current_time = timezone.now()
+    
+    return JsonResponse({
+        'timestamp': current_time.isoformat(),
+        'status': 'success'
+    })
